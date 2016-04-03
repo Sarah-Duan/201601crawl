c89f4898d585cb1b96df65e2f963902a5b1ac91d
zhangfengnodejs <83687401@qq.com>
2016-04-03 12:35:18 +0800

    成功入库

--- task/read.js
+++ task/read.js
@@ -1,4 +1,3 @@
-var uri = 'http://top.baidu.com/category?c=10&fr=topcategory_c10';
 
 var request = require('request');//拉取网页内容
 var cheerio = require('cheerio');//实现jquery功能
@@ -7,28 +6,32 @@ var iconv = require('iconv-lite');//把GBK转成UTF8
  * res 响应对象
  * body 响应体
  */
-request({url:uri,encoding:null},function(err,res,body){
-   if(err){
-       return console.error(err);
-   }
-    //把gbk编码的buffer转成utf8编码的字符串
-    body = iconv.decode(body,'gbk');
-    //根据响应体转成DOM对象
-    var $ = cheerio.load(body);
-    var items = [];
-    //找到所有的分类的A标签并进行转换
-    $('.hd .title a').each(function(){
-        var $me = $(this);
-        //<a href="http://top.baidu.com/buzz?b=353&amp;c=10&amp;fr=topcategory_c10">玄幻奇幻</a>
-        var item = {
-            name:$me.text().trim(),
-            url:$me.attr('href')
+exports.category = function(url,callback){
+    request({url:url,encoding:null},function(err,res,body){
+        if(err){
+            return console.error(err);
         }
-        var params = regParams(item.url);
-        item.id = params.b;
-        items.push(item);
+        //把gbk编码的buffer转成utf8编码的字符串
+        body = iconv.decode(body,'gbk');
+        //根据响应体转成DOM对象
+        var $ = cheerio.load(body);
+        var items = [];
+        //找到所有的分类的A标签并进行转换
+        $('.hd .title a').each(function(){
+            var $me = $(this);
+            //<a href="http://top.baidu.com/buzz?b=353&amp;c=10&amp;fr=topcategory_c10">玄幻奇幻</a>
+            var item = {
+                name:$me.text().trim(),
+                url:$me.attr('href')
+            }
+                var params = regParams(item.url);
+                item.id = params.b;
+                items.push(item);
+        });
+        callback(null,items);
     });
-});
+}
+
 
 function regParams(url) {
     var obj = {};
@@ -39,24 +42,29 @@ function regParams(url) {
     return obj
 }
 
-var articleUrl = 'http://top.baidu.com/buzz?b=355&c=10&fr=topcategory_c10';
-request({url:articleUrl,encoding:null},function(err,res,body){
-    if(err){
-        return console.error(err);
-    }
-    //把gbk编码的buffer转成utf8编码的字符串
-    body = iconv.decode(body,'gbk');
-    //根据响应体转成DOM对象
-    var $ = cheerio.load(body);
-    var items = [];
-    //找到所有的分类的A标签并进行转换
-    $('.keyword a').each(function(){
-        var $me = $(this);
-        var item = {
-            name:$me.text().trim(),
-            url:$me.attr('href')
+
+exports.article = function(url,cid,callback){
+    request({url:url,encoding:null},function(err,res,body){
+        if(err){
+            return console.error(err);
         }
-        console.log(item);
-        items.push(item);
+        //把gbk编码的buffer转成utf8编码的字符串
+        body = iconv.decode(body,'gbk');
+        //根据响应体转成DOM对象
+        var $ = cheerio.load(body);
+        var items = [];
+        //找到所有的分类的A标签并进行转换
+        $('.keyword a').each(function(){
+            var $me = $(this);
+            var item = {
+                name:$me.text().trim(),
+                url:$me.attr('href'),
+                cid:cid
+            }
+            if(item.name != 'search'){
+                items.push(item);
+            }
+        });
+        callback(null,items);
     });
-});
+}
---
+++ task/save.js
@@ -0,0 +1,27 @@
+var mysql = require('mysql');
+var pool = mysql.createPool({
+   host:'123.57.143.189',
+   user:'root',
+    password:'123456',
+    database:'crawl'
+});
+var async = require('async');
+//把分类列表存入数据库
+exports.category = function(list,callback){
+ async.forEach(list,function(item,cb){
+    pool.query('replace into category(id,name,url) values(?,?,?)',[item.id,item.name,item.url],function(err,result){
+        console.error(arguments);
+        cb();
+    });
+ },callback);
+}
+
+//把文章列表存入数据库
+exports.article = function(list,callback){
+    async.forEach(list,function(item,cb){
+        pool.query('replace into article(name,url,cid) values(?,?,?)',[item.name,item.url,item.cid],function(err,result){
+            console.error(arguments);
+            cb();
+        });
+    },callback);
+}
